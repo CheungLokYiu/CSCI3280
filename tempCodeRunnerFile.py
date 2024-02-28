2349fdc479a1a26162a11405c809d2fad9b6230d
CheungLokYiu <1155175425@link.cuhk.edu.hk>
2024-02-29 06:49:25 +0800

    hp

--- tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-    bytesperframe = int.from_bytes(nBlockAlign, 'little') // int.from_bytes(nchannels, 'little') #get bytes per frame to set read range
-    framerate = int.from_bytes(nSamplesPerSec, 'little') #get current framerate
-    new_audio_data = audio_data[int(start*bytesperframe*framerate):int(end*bytesperframe*framerate)] #get new_audio_data of editting range
-    nframe = int((end-start)*framerate) #get new no. of frame
-    datacksize = nframe * int.from_bytes(nBlockAlign, 'little') #get new datacksize
-    datacksize = struct.pack('<i', datacksize) #change dataacksize to bytes
-    framerate = int(framerate * speed) #get new framrate from speed
-    nSamplesPerSec = struct.pack('<i', framerate) #get nSamplesPerSec(bytes) from framerate(int)
---
+++ visualisz.py
@@ -0,0 +1,29 @@
+
+import matplotlib.pyplot as plt
+import wave
+import numpy as np
+import tkinter as tk
+from matplotlib.figure import Figure
+def plotSignalWave(song_file):
+	# open .wav file
+	sound_wave = wave.open(song_file, 'r')
+	
+	# extract raw audio from .wav file
+	signal = sound_wave.readframes(-1)
+	signal = np.frombuffer(signal, np.int16)
+
+	# frame rate of .wav file
+	frame_rate = sound_wave.getframerate()
+
+	# time vector spaced linearly with
+	# the size of the audio file
+	Time = np.linspace(0, len(signal)/frame_rate, num = len(signal))
+
+	# plot Time versus signal
+	# plt.title("Signal Wave")
+	plt.axis('off')
+	plt.plot(Time, signal)
+	plt.savefig('out.png', bbox_inches='tight',transparent=True, pad_inches=0)
+
+	return ''
+plotSignalWave('Output2.wav')
