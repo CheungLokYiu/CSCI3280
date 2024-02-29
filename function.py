501d36ce1ff34d5a9759dd28d31fc1d1af47a1c8
CheungLokYiu <1155175425@link.cuhk.edu.hk>
2024-02-29 23:24:27 +0800

    merge hp

--- function.py
+++ function.py
@@ -227,7 +227,7 @@ def savefile(outfilename, data):
     with open(outfilename, "wb") as f:
         f.write(data["raw_data"])
 
-def replace_audio(infilename, start, end, replace_data, old_data):
+def replace_audio(start, end, replace_data, old_data):
     framerate = old_data["framerate"]
     bytesperframe = old_data["bytesperframe"]
     old_audio_data = old_data["audio_data"]
