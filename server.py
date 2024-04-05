5d65f8f41a36592657a881339ce088a4d913a907
CheungLokYiu <1155175425@link.cuhk.edu.hk>
2024-04-05 22:20:28 +0800

    send ip address?

--- server.py
+++ server.py
@@ -1,46 +1,25 @@
-import threading
+import asyncio
+import websockets
 import socket
 
-host = '127.0.0.1'
-port = 59000
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((host, port))
-server.listen()
-clients = []
-aliases = []
-def broadcast(message):
-    for client in clients:
-        client.send(message)
+async def hello(websocket):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-def handle_client(client):
-    while True:
-        try:
-            message = client.recv(1024)
-            broadcast(message)
-        except:
-            index = clients.index(client)
-            clients.remove(client)
-            client.close()
-            alias = aliases[index]
-            broadcast(f'{alias} has left the chatroom!'.encode('utf-8'))
-            aliases.remove(alias)
-            break
+    # Connect the socket to a public DNS server's IP (Google's in this case)
+    s.connect(("8.8.8.8", 80))
 
-def receive():
-    while True:
-        print('Server is running and listening ...')
-        client, address = server.accept()
-        print(f'connection is established with {str(address)}')
-        client.send('alias?'.encode('utf-8'))
-        alias = client.recv(1024)
-        aliases.append(alias)
-        clients.append(client)
-        print(f'The alias of this client is {alias}'. encode('utf-8'))
-        broadcast(f'{alias} has connected to the chatroom'.encode('utf-8'))
-        client.send('you are now connected!'.encode('utf-8'))
+    # Get the socket's own IP address
+    ip = s.getsockname()[0]
 
-        thread = threading.Thread(target = handle_client, args=(client,))
-        thread.start()
+    # Close the dummy socket
+    s.close()
+
+    await websocket.send(ip)
+    print(f">>>{ip}")
+
+async def main():
+    async with websockets.serve(hello, "172.24.208.1", 8765):
+        await asyncio.Future()
 
 if __name__ == "__main__":
-    receive()
+    asyncio.run(main())
